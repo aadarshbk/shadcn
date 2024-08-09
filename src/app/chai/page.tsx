6fe1e417d67bf43f6d143f713cd063aa449b4f48
aadarshbk <babliaadarsh@gmail.com>
2024-08-09 09:40:50 +0545

    first commit

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function page() {
+   return (
+<main className="h-full flex justify-center items-center flex-col ">
+    <div>Aadarsh</div>
+    <button className="px-6 py-2 bg-blue--500 bg-blue-500 rounde my-3: hoover:bg-blue-400">Test button</button>
+    <Button value={'chai'}>Shad cn</Button>
+    </main >
+) 
+
+};
+
+export  default page;
